557f6191946ebf0efa401f530ea5acbcc5c01dbc
arkbot51 <rhahee528@gmail.com>
2025-01-08 12:14:25 -0800

    Our Mission component on About Page

--- src/components/about/Mission.tsx
+++ src/components/about/Mission.tsx
@@ -1,5 +1,20 @@
 const Mission = () => {
-  return <div>Mission</div>;
+  return (
+    <div className="flex w-full flex-col items-center py-10">
+      <p className="m-0 mb-6 text-center font-inika text-3xl font-bold sm:text-4xl">
+        OUR MISSION
+      </p>
+
+      <p className="m-0 w-3/5 text-center font-inika text-lg sm:w-2/5 sm:text-xl">
+        The mission of the Taekwondo Club is to provide training for those of
+        all skill levels. We welcome all those who have previous experience,
+        zero experience, are martial arts enthusiasts, or generally curious of
+        the sport. We aim to teach the rich Korean culture of Taekwondo to all
+        members, as well as the development of competitive skills while having
+        fun.
+      </p>
+    </div>
+  );
 };
 
 export default Mission;
