81510522d90b5d36d5266e48a7943539adce19f4
arkbot51 <rhahee528@gmail.com>
2025-01-09 21:11:21 -0800

    Our Mission Component-text made bigger

--- src/components/about/Mission.tsx
+++ src/components/about/Mission.tsx
@@ -1,11 +1,11 @@
 const Mission = () => {
   return (
     <div className="flex w-full flex-col items-center py-10">
-      <p className="m-0 mb-6 text-center font-inika text-3xl font-bold sm:text-4xl">
+      <p className="sm:leading[62.54] m-2 mb-8 text-center font-inika text-4xl font-bold sm:text-[48px]">
         OUR MISSION
       </p>
 
-      <p className="m-0 w-3/5 text-center font-inika text-lg sm:w-2/5 sm:text-xl">
+      <p className="m-0 w-4/5 text-center font-inika text-xl sm:m-3 sm:w-[869px] sm:text-[32px] sm:leading-[41.7px]">
         The mission of the Taekwondo Club is to provide training for those of
         all skill levels. We welcome all those who have previous experience,
         zero experience, are martial arts enthusiasts, or generally curious of
