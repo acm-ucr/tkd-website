3f63f4f40299c23d4de7e5d76764244a7caac2eb
amir <nintendothing123@gmail.com>
2025-01-10 14:48:02 -0800

    changed back width

--- src/components/events/EventCol.tsx
+++ src/components/events/EventCol.tsx
@@ -6,7 +6,7 @@ type eventProps = {
 
 const EventCol: React.FC<eventProps> = ({ event, description, time }) => {
   return (
-    <div className="justify-items-center space-y-6 md:w-1/4 lg:w-[15%]">
+    <div className="justify-items-center space-y-6 md:w-1/4 lg:w-1/6">
       <div className="w-full border-2 border-tkd-red-200 bg-tkd-red-100 py-2 text-center text-3xl text-white">
         {event}
       </div>
