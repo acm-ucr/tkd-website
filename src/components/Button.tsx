a510ab33979d607367e1a63a831a1b27abddce75
Luke Baring <baringluke35@gmail.com>
2025-01-11 09:18:53 -0800

    fixed preset custom props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -17,13 +17,13 @@ const Button = ({
   //dyanmic props to bg colors and border colors
   const bgVariant = {
     grey: "bg-grey-100",
-    red: "bg-red-300 hover:bg-red-200",
-    blue: "bg-blue-300 hover:bg-blue-200",
+    red: "bg-red-400 hover:bg-red-300",
+    blue: "bg-blue-500 hover:bg-blue-400",
   };
   const borderVariant = {
     grey: "border-grey-100",
-    red: "border-red-800 hover:border-red-600",
-    blue: "border-blue-800 hover:border-blue-600",
+    red: "border-red-800 hover:border-red-700",
+    blue: "border-blue-900 hover:border-blue-800",
   };
 
   //fall back to default colors if no provided colors
@@ -34,10 +34,10 @@ const Button = ({
     <Link
       href={link}
       target={target}
-      className={`font-inika-white box-border flex items-center justify-center border-4 border-solid px-7 py-8 text-xl lg:text-3xl ${bgClass} ${borderClass} `}
+      className={`box-border flex items-center justify-center border-4 border-solid px-7 py-8 font-inika text-xl font-medium lg:text-3xl ${bgClass} ${borderClass} `}
     >
       <div className="relative inline-block whitespace-nowrap">
-        <span className="flex justify-center text-black opacity-100">
+        <span className="flex justify-center text-white opacity-100">
           {txt}
         </span>
       </div>
