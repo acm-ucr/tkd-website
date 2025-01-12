2b9363b967ea66f0debcaf2316cedd484614ccb4
Andrew Chen <andrewseanchen@gmail.com>
2025-01-11 22:09:59 -0800

    gallery landing page

--- src/components/gallery/Landing.tsx
+++ src/components/gallery/Landing.tsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+import photo from "@/public/landing.webp";
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div className="relative flex flex-row items-center justify-center text-9xl">
+      <Image className="w-screen" src={photo} alt="Landing" />
+      <div className="absolute inset-0 flex flex-col items-center font-extralight">
+        <p className="relative right-[35%] top-[16%]">
+          Gallery
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
