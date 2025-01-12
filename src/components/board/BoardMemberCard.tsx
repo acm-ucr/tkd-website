85c6fa01fbb4c17e63766b4a899a0d9091997f01
Matthew Garcia <gmatthew2765@gmail.com>
2025-01-12 15:49:55 -0800

    Fixed white border bug and seperated data in anothe file.

--- src/components/board/BoardMemberCard.tsx
+++ src/components/board/BoardMemberCard.tsx
@@ -1,71 +1,18 @@
 import Image from "next/image";
-import Nick from "/public/Board/nick-nguyen.webp";
-import Leana from "/public/Board/leanna-riberal.webp";
-import Eddie from "/public/Board/eddie-park.webp";
-import Ashley from "/public/Board/ashley-chan.webp";
-import Aaron from "/public/Board/aaron-choi.webp";
-import Liam from "/public/Board/liam-blanchard.webp";
-import Santiago from "/public/Board/santiago-trejo.webp";
-import Chaele from "/public/Board/chaele-kim.webp";
 
-const BoardMemberCard = () => {
-  const boardMembers = [
-    {
-      url: Nick,
-      position: "President",
-      name: "Nick Nguyen",
-      id: 1,
-    },
-    {
-      url: Leana,
-      position: "Vice President",
-      name: "Leana Riberal",
-      id: 2,
-    },
-    {
-      url: Eddie,
-      position: "Treasurer",
-      name: "Eddie Park",
-      id: 3,
-    },
-    {
-      url: Ashley,
-      position: "Secretary",
-      name: "Ashley Chan",
-      id: 4,
-    },
-    {
-      url: Aaron,
-      position: "Social Chair/Instructor",
-      name: "Aaron Choi",
-      id: 5,
-    },
-    {
-      url: Liam,
-      position: "Coach/Instructor",
-      name: "Liam Blanchard",
-      id: 6,
-    },
-    {
-      url: Santiago,
-      position: "Coach/Instructor",
-      name: "Santiago Trejo",
-      id: 7,
-    },
-    {
-      url: Chaele,
-      position: "Coach/Instructor",
-      name: "Chaele Kim",
-      id: 8,
-    },
-  ];
-
-  const renderBoard = boardMembers.map((member) => (
-    <div key={member.id}>
-      <div className="border-1 relative z-[1] h-72 w-60 border-2 border-white">
-        <Image src={member.url} fill objectFit="cover" className="m-3" />
+const BoardMemberCard = ({ members }) => {
+  const renderBoard = members.map((member) => (
+    <div key={member.id} className="relative">
+      <div className="absolute bottom-4 right-4 z-[2] h-72 w-60 border-[1px] border-white"></div>
+      <div className="relative z-[1] h-72 w-60">
+        <Image
+          src={member.url}
+          alt={member.name}
+          fill
+          style={{ objectFit: "cover" }}
+        />
       </div>
-      <div className="relative bottom-6 left-14 z-[2] w-fit rounded-md bg-tkd-blue-100 px-5 text-center font-inika">
+      <div className="absolute -bottom-5 left-10 z-[3] h-fit w-fit rounded-md bg-tkd-blue-100 px-5 text-center font-inika text-lg">
         <p>{member.name}</p>
         <p>{member.position}</p>
       </div>
@@ -73,9 +20,7 @@ const BoardMemberCard = () => {
   ));
 
   return (
-    <div className="flex flex-wrap justify-center gap-x-10 gap-y-1">
-      {renderBoard}
-    </div>
+    <div className="flex flex-wrap justify-center gap-20">{renderBoard}</div>
   );
 };
 
