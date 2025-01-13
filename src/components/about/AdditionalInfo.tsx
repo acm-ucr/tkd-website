bf862f8c7441df8a8c4f54c9dcec380333145163
Rebecca-J7 <rjenn003@ucr.edu>
2025-01-12 18:39:58 -0800

    added centering to text in the blue box

--- src/components/about/AdditionalInfo.tsx
+++ src/components/about/AdditionalInfo.tsx
@@ -15,17 +15,17 @@ const AdditionalInfo = () => {
       </p>
       <div className="right-[20%] top-[40%] h-[40%] w-[54%] border-2 border-white bg-transparent"></div>
       <div className="absolute right-[11%] top-[45%] h-[35%] w-[55%] bg-tkd-blue-200">
-        <div className="absolute -top-[9%] right-[2.7%] h-[100%] w-[100%] border-4 border-white bg-transparent"></div>
-        <p className="relative left-[13%] top-3 text-2xl font-semibold text-white">
+        <div className="absolute -top-[9%] right-[2%] h-[100%] w-[100%] border-4 border-white bg-transparent"></div>
+        <p className="relative top-3 text-2xl font-semibold text-white flex justify-center">
           How often does your student organization meet?
         </p>
-        <p className="relative left-[40%] top-3 text-2xl font-medium text-white">
+        <p className="relative top-3 text-2xl font-medium text-white flex justify-center">
           Twice a week.
         </p>
-        <p className="relative left-1 top-10 text-2xl font-semibold text-white">
+        <p className="relative top-10 text-2xl font-semibold text-white flex justify-center">
           What day and time does your student organization generally meet?
         </p>
-        <p className="relative left-[16%] top-10 text-2xl font-medium text-white">
+        <p className="relative top-10 text-2xl font-medium text-white flex justify-center">
           Mondays and Wednesdays from 9:30pm-10:50pm
         </p>
       </div>
