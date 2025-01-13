33d0c5bc96006e8dd9a077f4a3ea4bf3e5f0c11f
Haokun Yang <haokunyang@Mac.lan>
2025-01-12 18:39:15 -0800

    landingSection Change

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -1,20 +1,20 @@
 import React from "react";
-import BackgroundImage from "@/public/landing/DSC07563.webp";
+import BackgroundImage from "@/public/landing/postcss.config.webp";
 import Image from "next/image";
 
 const Landing = () => {
   return (
     <div className="relative">
       <Image
-        className="w-screen"
+        className="w-screen h-screen brightness-50"
         src={BackgroundImage}
         alt="background landing page"
       />
       <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
-        <div className="relative bottom-[25%] right-[25%] font-caveat-brush text-9xl">
+        <div className="relative bottom-[34%] right-[25%] font-caveat-brush text-9xl">
           Taekwondo @ UCR
         </div>
-        <div className="relative left-[25%] top-[25%] font-inika text-5xl">
+        <div className="relative left-[26%] top-[36%] font-inika text-5xl">
           Welcome to Taekwondo Club at UCR!
         </div>
       </div>
