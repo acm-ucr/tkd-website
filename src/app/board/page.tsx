54a5db592b1f5095afbe77f4e035e6a1d1e7b52e
David <davidbjolee@gmail.com>
2025-01-12 17:36:17 -0800

    Added Board Landing component

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -3,7 +3,7 @@ import Landing from "@/components/board/Landing";
 const Board = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <Landing/>
+      <Landing />
     </div>
   );
 };
--- src/components/board/Landing.tsx
+++ src/components/board/Landing.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
 import { ArrowDown } from "lucide-react";
 
-
 const Landing = () => {
   return (
     <div className="w-screen">
-      <div className="absolute grid flex-row right-0 items-center justify-center top-0 w-2/3">
+      <div className="absolute right-0 top-0 grid w-2/3 flex-row items-center justify-center">
         <Image
           src="/board/boardGroupImage.webp"
           width={4000}
@@ -15,21 +14,17 @@ const Landing = () => {
         />
       </div>
       <div className="absolute left-[60%] top-[13%] 2xl:left-[61%] 2xl:top-[47%]">
-        <p className="font-caveat-brush text-white text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-8xl">
+        <p className="font-caveat-brush text-3xl text-white md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-8xl">
           BOARD
         </p>
       </div>
-      <div className="absolute flex-row left-0 top-[12%] items-center justify-center w-1/3 grid text-3xl md:text-4xl lg:text-5xl lg:top-[23%] xl:text-6xl xl:xl-[24%] 2xl:text-7xl 2xl:top-[25%]">
-        <div className="absolute right-[42.75%] top-[105%]"><ArrowDown size={80}/></div>
-        <p className="relative text-center mb-[10%]">
-          MEET
-        </p>
-        <p className="relative text-center mb-[10%]">
-          THE
-        </p>
-        <p className="relative text-center">
-          BOARD
-        </p>
+      <div className="xl:xl-[24%] absolute left-0 top-[12%] grid w-1/3 flex-row items-center justify-center text-3xl md:text-4xl lg:top-[23%] lg:text-5xl xl:text-6xl 2xl:top-[25%] 2xl:text-7xl">
+        <div className="absolute right-[42.75%] top-[105%]">
+          <ArrowDown size={80} />
+        </div>
+        <p className="relative mb-[10%] text-center">MEET</p>
+        <p className="relative mb-[10%] text-center">THE</p>
+        <p className="relative text-center">BOARD</p>
       </div>
     </div>
   );
