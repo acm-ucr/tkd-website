ffc024e45ed774b9cbcc024a67e5fdb0336291e6
Rebecca-J7 <rjenn003@ucr.edu>
2025-01-13 21:38:08 -0800

    fixed changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,8 @@
-import AdditionalInfo from "@/components/about/AdditionalInfo";
 import Mission from "@/components/about/Mission";
 
 const About = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <AdditionalInfo />
       <Mission />
     </div>
   );
