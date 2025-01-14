4ae9660372fc001faf491a257d046f5ae0164339
Haokun Yang <haokunyang@ucr-secure-20-10-13-129-241.wnet.ucr.edu>
2025-01-14 11:33:56 -0800

    overflow fix?

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -6,15 +6,15 @@ const Landing = () => {
   return (
     <div className="relative">
       <Image
-        className="h-screen w-screen brightness-50"
+        className="w-screen brightness-50"
         src={BackgroundImage}
         alt="background landing page"
       />
-      <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
-        <div className="relative bottom-[34%] right-[25%] font-caveat-brush text-9xl">
-          Taekwondo @ UCR
+      <div className="items-center justify-center text-white">
+        <div className="absolute left-[7%] top-[9%] font-caveat-brush text-9xl">
+          TAEKWONDO @ UCR
         </div>
-        <div className="relative left-[26%] top-[36%] font-inika text-5xl">
+        <div className="absolute bottom-[5%] right-[4%] font-inika text-5xl">
           Welcome to Taekwondo Club at UCR!
         </div>
       </div>
