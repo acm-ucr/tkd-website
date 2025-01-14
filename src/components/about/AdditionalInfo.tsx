03651ef5ceafaee49a63cf6e4dc2f84f0e86dc57
Rebecca-J7 <rjenn003@ucr.edu>
2025-01-13 16:48:12 -0800

    stash

--- src/components/about/AdditionalInfo.tsx
+++ src/components/about/AdditionalInfo.tsx
@@ -16,16 +16,16 @@ const AdditionalInfo = () => {
       <div className="right-[20%] top-[40%] h-[40%] w-[54%] border-2 border-white bg-transparent"></div>
       <div className="absolute right-[11%] top-[45%] h-[35%] w-[55%] bg-tkd-blue-200">
         <div className="absolute -top-[9%] right-[2%] h-[100%] w-[100%] border-4 border-white bg-transparent"></div>
-        <p className="relative top-3 text-2xl font-semibold text-white flex justify-center">
+        <p className="relative top-3 flex justify-center text-2xl font-semibold text-white">
           How often does your student organization meet?
         </p>
-        <p className="relative top-3 text-2xl font-medium text-white flex justify-center">
+        <p className="relative top-3 flex justify-center text-2xl font-medium text-white">
           Twice a week.
         </p>
-        <p className="relative top-10 text-2xl font-semibold text-white flex justify-center">
+        <p className="relative top-10 flex justify-center text-2xl font-semibold text-white">
           What day and time does your student organization generally meet?
         </p>
-        <p className="relative top-10 text-2xl font-medium text-white flex justify-center">
+        <p className="relative top-10 flex justify-center text-2xl font-medium text-white">
           Mondays and Wednesdays from 9:30pm-10:50pm
         </p>
       </div>
