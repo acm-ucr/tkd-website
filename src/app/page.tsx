89209e5a38e54da11f80eaaa2ff2b60b48cd12ed
Haokun Yang <haokunyang@ucr-secure-20-10-13-128-207.wnet.ucr.edu>
2025-01-14 19:07:03 -0800

    minor change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import LandingPage from "@/components/home/Landing";
 import React from "react";
 
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <LandingPage />
     </div>
   );
 };
--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -11,10 +11,10 @@ const Landing = () => {
         alt="background landing page"
       />
       <div className="items-center justify-center text-white">
-        <div className="absolute left-[7%] top-[9%] font-caveat-brush text-9xl">
+        <div className="absolute left-[5%] top-[9%] font-caveat-brush text-9xl">
           TAEKWONDO @ UCR
         </div>
-        <div className="absolute bottom-[5%] right-[4%] font-inika text-5xl">
+        <div className="absolute bottom-[8%] right-[4%] font-inika text-5xl">
           Welcome to Taekwondo Club at UCR!
         </div>
       </div>
