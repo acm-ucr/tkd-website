0c940a395415f8af2b9eea551dd6301896f8567e
Matthew Garcia <gmatthew2765@gmail.com>
2025-01-14 22:08:27 -0800

    Fixed typos

--- src/components/board/BoardMembers.tsx
+++ src/components/board/BoardMembers.tsx
@@ -6,7 +6,11 @@ import BoardMemberCard from "@/components/board/BoardMemberCard";
 // 1) Import the BoardMembers component into your target file alongside with { BoardData }
 // 2) Use the component and pass in the BoardData as a prop <BoardMembers members={BoardData} />
 
-const BoardMembers = ({ members }: { members: Member[] }) => {
+interface BoardMembersProps {
+  members: Member[];
+}
+
+const BoardMembers = ({ members }: BoardMembersProps) => {
   return (
     <div className="flex flex-wrap justify-center gap-20">
       {members.map((member) => (
