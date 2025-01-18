9894a6303895501d7726c3672d2b824e3d3b6615
amir <nintendothing123@gmail.com>
2025-01-17 18:12:29 -0800

    added Sparring Gear

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import SparringGear from "@/components/home/SparringGear";
+
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      Home
+    <div className="flex h-screen w-screen items-center justify-center bg-black">
+      <SparringGear
+        Title="Need Sparring Gear?"
+        buttonTxt="Purchase Gear"
+        link="_blank"
+      />
     </div>
   );
 };
--- src/components/home/SparringGear.tsx
+++ src/components/home/SparringGear.tsx
@@ -1,5 +1,30 @@
-const SparringGear = () => {
-  return <div>Sparring Gear</div>;
+import Button from "@/components/Button";
+
+type sparringGearProps = {
+  Title: string;
+  buttonTxt: string;
+  link: string;
+};
+
+const SparringGear: React.FC<sparringGearProps> = ({
+  Title,
+  buttonTxt,
+  link,
+}) => {
+  return (
+    <div className="md:5xl justify-items-center space-y-24 text-6xl font-bold text-white">
+      <div>{Title}</div>
+      <div className="">
+        <Button
+          txt={buttonTxt}
+          link={link}
+          bgColor="red"
+          borderColor="red"
+          target="_blank"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SparringGear;
