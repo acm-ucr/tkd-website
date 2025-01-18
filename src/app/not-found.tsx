8720a78fad2bcfd73d774535a0469cf1b893696b
arkbot51 <rhahee528@gmail.com>
2025-01-17 16:39:33 -0800

    changed background image to Image, made divs bigger, and added textSize and size to Button component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,28 +1,34 @@
 import Button from "@/components/Button";
+import Image from "next/image";
 
 const NotFound = () => {
   return (
-    <div
-      className="h-screen w-screen bg-cover bg-center"
-      style={{ backgroundImage: "url('/404page-background.webp')" }}
-    >
-      <div className="absolute left-[50%] top-[50%] z-0 flex h-[50%] w-[55%] translate-x-[-50%] translate-y-[-50%] items-center justify-center bg-tkd-blue-100">
-        <div className="absolute -top-[8%] right-[4%] h-[100%] w-[100%] border-2 border-white bg-transparent">
-          <p className="absolute left-[50%] top-[-15%] translate-x-[-50%] translate-y-[5%] font-caveat-brush text-9xl text-white">
+    <div className="h-screen w-screen bg-cover bg-center">
+      <Image
+        src="/404page-background.webp"
+        width={3024}
+        height={1776}
+        className="h-auto max-h-screen w-screen object-cover"
+        alt=" 404 background image"
+      />
+      <div className="absolute left-[50%] top-[50%] z-0 flex h-[60%] w-[65%] translate-x-[-50%] translate-y-[-50%] items-center justify-center bg-tkd-blue-100">
+        <div className="absolute -top-[7%] right-[3%] h-[100%] w-[100%] border-2 border-white bg-transparent">
+          <p className="absolute left-[50%] top-[-15%] translate-x-[-50%] translate-y-[-10%] font-caveat-brush text-[860%] text-white">
             404
           </p>
         </div>
-        <p className="translate-y-[-10%] font-inika text-7xl text-white">
+        <p className="translate-y-[-11%] font-inika text-[550%] text-white">
           PAGE NOT FOUND
         </p>
       </div>
-      <div className="absolute left-[50%] top-[78%] h-[10%] w-[17%] translate-x-[-50%]">
+      <div className="absolute left-[50%] top-[82%] h-[11%] w-[20%] translate-x-[-50%]">
         <Button
           txt="Back To Home"
           link="/"
           borderColor="red"
           bgColor="red"
           size="w-[100%] h-[100%]"
+          textSize="text-2xl lg:text-4xl"
         />
       </div>
     </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,6 +8,7 @@ const Button = ({
   borderColor = "grey", //defaults to "grey" border class if no colors provided
   bgColor = "grey", //defaults to "grey" bg class if no colors provided
   size = "", //defaults to empty string if no size is provided
+  textSize = "text-xl lg:text-3xl",
 }: {
   txt: string;
   target?: string;
@@ -15,6 +16,7 @@ const Button = ({
   borderColor?: "red" | "blue" | "grey";
   bgColor?: "red" | "blue" | "grey";
   size?: string;
+  textSize?: string;
 }) => {
   //dyanmic props to bg colors and border colors
   const bgVariant = {
@@ -36,7 +38,7 @@ const Button = ({
     <Link
       href={link}
       target={target}
-      className={`box-border flex items-center justify-center border-4 border-solid px-7 py-7 font-inika text-xl font-medium lg:text-3xl ${bgClass} ${borderClass} ${size}`}
+      className={`box-border flex items-center justify-center border-4 border-solid px-7 py-7 font-inika font-medium ${textSize} ${bgClass} ${borderClass} ${size}`}
     >
       <div className="relative inline-block whitespace-nowrap">
         <span className="flex justify-center text-white opacity-100">
