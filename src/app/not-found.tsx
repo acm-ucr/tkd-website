c2db613fd95612cef51c31c11bc850912571ee2c
arkbot51 <rhahee528@gmail.com>
2025-01-19 15:57:37 -0800

    reduced blue box size by 5%, changed text sizes to shrink with the screen, increased button size

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,24 +11,24 @@ const NotFound = () => {
         className="h-auto max-h-screen w-screen object-cover"
         alt=" 404 background image"
       />
-      <div className="absolute left-[50%] top-[50%] z-0 flex h-[60%] w-[65%] translate-x-[-50%] translate-y-[-50%] items-center justify-center bg-tkd-blue-100">
+      <div className="absolute left-[50%] top-[50%] z-0 flex h-[55%] w-[60%] translate-x-[-50%] translate-y-[-50%] items-center justify-center bg-tkd-blue-100">
         <div className="absolute -top-[7%] right-[3%] h-[100%] w-[100%] border-2 border-white bg-transparent">
-          <p className="absolute left-[50%] top-[-15%] translate-x-[-50%] translate-y-[-10%] font-caveat-brush text-[860%] text-white">
+          <p className="absolute left-[50%] top-[-15%] translate-x-[-50%] translate-y-[-10%] font-caveat-brush text-[9.5vw] text-white">
             404
           </p>
         </div>
-        <p className="translate-y-[-11%] font-inika text-[550%] text-white">
+        <p className="translate-y-[-11%] font-inika text-[6vw] text-white">
           PAGE NOT FOUND
         </p>
       </div>
-      <div className="absolute left-[50%] top-[82%] h-[11%] w-[20%] translate-x-[-50%]">
+      <div className="absolute left-[50%] top-[82%] h-[12%] w-[22%] translate-x-[-50%]">
         <Button
           txt="Back To Home"
           link="/"
           borderColor="red"
           bgColor="red"
           size="w-[100%] h-[100%]"
-          textSize="text-2xl lg:text-4xl"
+          textSize="text-[3vw]"
         />
       </div>
     </div>
@@ -36,3 +36,4 @@ const NotFound = () => {
 };
 
 export default NotFound;
+
