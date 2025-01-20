1fc26b83420915057a79ca218cac776926e9680f
amir <nintendothing123@gmail.com>
2025-01-20 14:58:12 -0800

    added page may need adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,6 @@
-import SparringGear from "@/components/home/SparringGear";
-
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-black">
-      <SparringGear
-        Title="Need Sparring Gear?"
-        buttonTxt="Purchase Gear"
-        link="_blank"
-      />
-    </div>
+    <div className="flex h-screen w-screen items-center justify-center"></div>
   );
 };
 
--- src/components/home/SparringGear.tsx
+++ src/components/home/SparringGear.tsx
@@ -1,27 +1,44 @@
 import Button from "@/components/Button";
+import gearPhoto from "@/public/home/sparringGear.webp";
+import Image from "next/image";
 
 type sparringGearProps = {
-  Title: string;
   buttonTxt: string;
   link: string;
 };
 
-const SparringGear: React.FC<sparringGearProps> = ({
-  Title,
-  buttonTxt,
-  link,
-}) => {
+const SparringGear: React.FC<sparringGearProps> = ({ buttonTxt, link }) => {
   return (
-    <div className="md:5xl justify-items-center space-y-24 text-6xl font-bold text-white">
-      <div>{Title}</div>
-      <div className="">
-        <Button
-          txt={buttonTxt}
-          link={link}
-          bgColor="red"
-          borderColor="red"
-          target="_blank"
-        />
+    <div className="relative w-fit justify-items-center font-bold text-white md:text-3xl lg:text-7xl">
+      <Image
+        src={gearPhoto}
+        alt="sparringGearPhoto"
+        className="w-screen brightness-50"
+      />
+      <div className="absolute inset-0 flex flex-col items-center justify-center space-y-5 lg:space-y-20">
+        <div>Need Sparring Gear</div>
+        <div className="">
+          <Button
+            txt={buttonTxt}
+            link={link}
+            bgColor="red"
+            borderColor="red"
+            target="_blank"
+          />
+        </div>
+        <br></br>
+        <div className="absolute -bottom-[-5%] right-5 flex w-1/2 flex-col items-end space-y-4 md:text-2xl lg:right-20 lg:text-4xl">
+          <div className="mx-10">Want a Jacket?</div>
+          <div className="">
+            <Button
+              txt="Purchase Jacket"
+              link={link}
+              bgColor="red"
+              borderColor="red"
+              target="_blank"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
