3e3d1269890e2196036897a6f810aa4b6fc5aafb
Rebecca-J7 <rjenn003@ucr.edu>
2025-01-19 21:42:33 -0800

    responsive

--- src/components/about/AdditionalInfo.tsx
+++ src/components/about/AdditionalInfo.tsx
@@ -2,32 +2,33 @@ import Image from "next/image";
 
 const AdditionalInfo = () => {
   return (
-    <div className="w-screen">
+    <div className="relative w-full overflow-hidden">
       <Image
         src="/about/background.webp"
         width={1700}
         height={1080}
-        className="max-h-full w-screen"
+        className="h-auto w-full object-cover"
         alt=" background image"
       />
-      <p className="absolute left-[13%] top-[25%] font-inika text-4xl font-semibold text-white">
+
+      <p className="absolute left-[12%] top-[20%] font-inika font-semibold text-white sm:text-3xl md:text-3xl lg:text-4xl">
         Additional Information:
       </p>
-      <div className="right-[20%] top-[40%] h-[40%] w-[54%] border-2 border-white bg-transparent"></div>
-      <div className="absolute right-[11%] top-[45%] h-[35%] w-[55%] bg-tkd-blue-200">
-        <div className="absolute -top-[9%] right-[2%] h-[100%] w-[100%] border-4 border-white bg-transparent"></div>
-        <p className="relative top-3 flex justify-center text-2xl font-semibold text-white">
-          How often does your student organization meet?
-        </p>
-        <p className="relative top-3 flex justify-center text-2xl font-medium text-white">
-          Twice a week.
-        </p>
-        <p className="relative top-10 flex justify-center text-2xl font-semibold text-white">
-          What day and time does your student organization generally meet?
-        </p>
-        <p className="relative top-10 flex justify-center text-2xl font-medium text-white">
-          Mondays and Wednesdays from 9:30pm-10:50pm
-        </p>
+
+      <div className="absolute right-[6%] top-[46%] h-[37%] w-[58%] bg-tkd-blue-200">
+        <div className="absolute -top-[9%] right-[3%] h-[100%] w-[100%] border-4 border-white bg-transparent"></div>
+        <div className="text-wrap text-center text-white sm:text-base md:text-lg lg:text-xl">
+          <p className="relative top-4 font-semibold">
+            How often does your student organization meet?
+          </p>
+          <p className="relative top-4 font-medium">Twice a week.</p>
+          <p className="relative top-11 font-semibold">
+            What day and time does your student organization generally meet?
+          </p>
+          <p className="relative top-11 font-medium">
+            Mondays and Wednesdays from 9:30pm-10:50pm
+          </p>
+        </div>
       </div>
     </div>
   );
