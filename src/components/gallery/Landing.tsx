3346bc8e45e8874d43bb5664546df0c8bea0ce30
Andrew Chen <andrewseanchen@gmail.com>
2025-01-20 22:13:04 -0800

    image resize

--- src/components/gallery/Landing.tsx
+++ src/components/gallery/Landing.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import photo from "@/../public/gallery/landing.webp";
+import landing from "@/../public/gallery/landing.webp";
 
 const Landing = () => {
   return (
     <div className="relative flex flex-row items-center justify-center text-9xl">
-      <Image className="w-screen" src={photo} alt="Landing" />
+      <Image className="w-screen" src={landing} alt="Landing" />
       <div className="absolute inset-0 flex flex-col items-center font-caveat-brush">
         <p className="relative left-[25%] top-[9%] text-white">Gallery</p>
       </div>
