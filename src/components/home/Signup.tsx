e80caf8931388250ca96808f50a83ac39ed9b15d
Haokun Yang <haokunyang@Mac.lan>
2025-01-20 19:35:36 -0800

    member signup

--- src/components/home/Signup.tsx
+++ src/components/home/Signup.tsx
@@ -1,5 +1,30 @@
+import SignUpButton from "@/components/home/SignupButton";
+import Image from "next/image";
+import Totoro from "@/../public/home/Rectangle-21.webp";
+
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div className="m-10 p-10">
+      <div className="font-Inika pb-8 text-center text-5xl font-bold text-black">
+        Interested in Becoming a Member?
+      </div>
+      <div className="flex flex-row  justify-between">
+        <div className="ml-12 mt-8 justify-between pl-12">
+          <div className="font-Inika mb-10 pb-10 text-5xl leading-relaxed text-black">
+            blahblahblahblahblahblahblahblah <br />
+            blahblahblah <br />
+            blahblahblah
+          </div>
+          <div className="justify-self-center pb-10">
+            <SignUpButton />
+          </div>
+        </div>
+        <div className="pt-10 pr-10 mr-12">
+          <Image src={Totoro} alt="Totoro in the rain" className="w-full" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
--- src/components/home/SignupButton.tsx
+++ src/components/home/SignupButton.tsx
@@ -1,5 +1,21 @@
+'use client';
+import React from 'react';
+
 const SignupButton = () => {
-  return <div>Signup Button</div>;
+  const handleClick = () => {
+    window.location.href = 'https://youtube.com';
+  };
+
+  return (
+    <div>
+      <button 
+        onClick={handleClick}
+        className="bg-blue-500 hover:bg-blue-800 text-white text-5xl font-inika py-6 px-10 border-4 border-black"
+      >
+        Sign Up
+      </button>
+    </div>
+  );
 };
 
 export default SignupButton;
