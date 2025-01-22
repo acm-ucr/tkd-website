40bb0ee50ddf1a714994baa3f60327b27e35b623
Haokun Yang <haokunyang@Mac.lan>
2025-01-21 17:48:09 -0800

    remove component from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Landing from "@/components/home/Landing";
-import Signup from "@/components/home/Signup";
 
 const Home = () => {
   return (
     <div className="">
       <Landing />
-      <Signup />
     </div>
   );
 };
