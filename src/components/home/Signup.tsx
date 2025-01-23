bf835f2f7b7a2b1bff40b6f21d39b8b5f615a413
Haokun Yang <haokunyang@Mac.lan>
2025-01-22 23:50:52 -0800

    changes 4?

--- src/components/home/Signup.tsx
+++ src/components/home/Signup.tsx
@@ -10,23 +10,23 @@ const Signup = () => {
       </div>
       <div className="flex flex-row items-center justify-between">
         <div className="ml-5 mt-8 pl-12">
-          <div className="font-Inika mb-6 pb-28 text-5xl pl-24 leading-relaxed text-black">
+          <div className="font-Inika mb-6 pb-28 pl-24 text-5xl leading-relaxed text-black">
             blahblahblahblahblahblahblahblah <br />
             blahblahblah <br />
             blahblahblah
           </div>
           <div className="justify-self-center pl-16 pt-16">
             <SignUpButton
-            txt="Sign Up" 
-            link="/some-page" 
-            borderColor="blue" 
-            bgColor="blue" 
-            size="w-72 h-32"
-            textSize="text-5xl"
-             />
+              txt="Sign Up"
+              link="/some-page"
+              borderColor="blue"
+              bgColor="blue"
+              size="w-72 h-32"
+              textSize="text-5xl"
+            />
           </div>
         </div>
-        <div className=" w-[46%] pl-12 pt-10">
+        <div className="w-[46%] pl-12 pt-10">
           <Image src={GroupPhoto} alt="group photo" className="rounded-xl" />
         </div>
       </div>
