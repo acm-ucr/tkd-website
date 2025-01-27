cac5b2c912205670eefda6e370a84c10b9001f50
Rebecca Jennings <157240153+Rebecca-J7@users.noreply.github.com>
2025-01-27 11:51:43 -0800

    added responsive features

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,8 @@
-import Mission from "@/components/about/Mission";
+
 
 const About = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <Mission />
     </div>
   );
 };
--- src/components/about/AdditionalInfo.tsx
+++ src/components/about/AdditionalInfo.tsx
@@ -7,27 +7,30 @@ const AdditionalInfo = () => {
         src="/about/background.webp"
         width={1700}
         height={1080}
-        className="h-auto w-full object-cover"
+        className="h-auto w-full object-cover brightness-50"
         alt=" background image"
       />
 
-      <p className="absolute left-[12%] top-[20%] font-inika font-semibold text-white sm:text-3xl md:text-3xl lg:text-4xl">
+      <p className="absolute left-[12%] top-[20%] font-inika font-semibold text-white sm:text-sm md:text-lg lg:text-4xl">
         Additional Information:
       </p>
 
-      <div className="absolute right-[6%] top-[46%] h-[37%] w-[58%] bg-tkd-blue-200">
-        <div className="absolute -top-[9%] right-[3%] h-[100%] w-[100%] border-4 border-white bg-transparent"></div>
-        <div className="text-wrap text-center text-white sm:text-base md:text-lg lg:text-xl">
-          <p className="relative top-4 font-semibold">
+      <div className="absolute right-[11%] top-[44%] h-[40%] w-[55%] bg-tkd-blue-200">
+        <div className=" relative -top-[9%] right-[3%] h-[100%] w-[100%] border-4 border-white bg-transparent flex justify-center">
+          <div className="text-white sm:text-cs md:text-base lg:text-xl justify-items-center relative top-[20%]">
+          <p className="font-semibold">
             How often does your student organization meet?
           </p>
-          <p className="relative top-4 font-medium">Twice a week.</p>
-          <p className="relative top-11 font-semibold">
+          <p className="font-medium">
+            Twice a week.
+          </p>
+          <p className="font-semibold">
             What day and time does your student organization generally meet?
           </p>
-          <p className="relative top-11 font-medium">
+          <p className="font-medium">
             Mondays and Wednesdays from 9:30pm-10:50pm
           </p>
+          </div>
         </div>
       </div>
     </div>
