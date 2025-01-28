dfe89762bac21fe87aabc85096d78aeea95dc61c
Daniyal Ahmed <dahme007@ucr.edu>
2025-01-28 09:37:59 -0800

    fixed linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,12 @@ import Landing from "@/components/home/Landing";
 import SparringGear from "@/components/home/SparringGear";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="">
+      <Landing />
+      <SparringGear />
+    </div>
+  );
 };
 
 export default Home;
