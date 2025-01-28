57ecd619a897c408c9401f58a521166db33a1f6e
David <davidbjolee@gmail.com>
2025-01-28 13:01:26 -0800

    Updated Board Landing component according to feedback

--- src/components/board/Landing.tsx
+++ src/components/board/Landing.tsx
@@ -3,15 +3,15 @@ import { ArrowDown } from "lucide-react";
 
 const Landing = () => {
   return (
-    <div className="w-screen h-screen flex">
-      <div className="w-1/3 bg-white flex flex-col items-center justify-center text-center p-4">
-        <p className="font-caveat-brush text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl mb-4">
+    <div className="flex h-screen w-screen">
+      <div className="flex w-1/3 flex-col items-center justify-center bg-white p-4 text-center">
+        <p className="mb-4 font-caveat-brush text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
           MEET
         </p>
-        <p className="font-caveat-brush text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl mb-4">
+        <p className="mb-4 font-caveat-brush text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
           THE
         </p>
-        <p className="font-caveat-brush text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl mb-8">
+        <p className="mb-8 font-caveat-brush text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
           BOARD
         </p>
         <div className="mt-4">
@@ -19,8 +19,8 @@ const Landing = () => {
         </div>
       </div>
 
-      <div className="w-2/3 relative">
-        <div className="absolute inset-0 w-full h-full">
+      <div className="relative w-2/3">
+        <div className="absolute inset-0 h-full w-full">
           <Image
             src="/board/boardGroupImage.webp"
             alt="board group image"
@@ -30,7 +30,7 @@ const Landing = () => {
           />
         </div>
 
-        <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
           <p className="font-caveat-brush text-3xl text-white md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-8xl">
             BOARD
           </p>
