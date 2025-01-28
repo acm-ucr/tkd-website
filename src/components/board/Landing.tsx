de0304f21ac22cd940c891e7d652a353552a1d6e
David <davidbjolee@gmail.com>
2025-01-28 13:00:39 -0800

    Updated Board Landing component according to feedback

--- src/components/board/Landing.tsx
+++ src/components/board/Landing.tsx
@@ -3,28 +3,38 @@ import { ArrowDown } from "lucide-react";
 
 const Landing = () => {
   return (
-    <div className="w-screen">
-      <div className="absolute right-0 top-0 grid w-2/3 flex-row items-center justify-center">
-        <Image
-          src="/board/boardGroupImage.webp"
-          width={4000}
-          height={4000}
-          className="brightness-50"
-          alt=" background image"
-        />
-      </div>
-      <div className="absolute left-[60%] top-[13%] 2xl:left-[61%] 2xl:top-[47%]">
-        <p className="font-caveat-brush text-3xl text-white md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-8xl">
+    <div className="w-screen h-screen flex">
+      <div className="w-1/3 bg-white flex flex-col items-center justify-center text-center p-4">
+        <p className="font-caveat-brush text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl mb-4">
+          MEET
+        </p>
+        <p className="font-caveat-brush text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl mb-4">
+          THE
+        </p>
+        <p className="font-caveat-brush text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl mb-8">
           BOARD
         </p>
-      </div>
-      <div className="xl:xl-[24%] absolute left-0 top-[12%] grid w-1/3 flex-row items-center justify-center text-3xl md:text-4xl lg:top-[23%] lg:text-5xl xl:text-6xl 2xl:top-[25%] 2xl:text-7xl">
-        <div className="absolute right-[42.75%] top-[105%]">
+        <div className="mt-4">
           <ArrowDown size={80} />
         </div>
-        <p className="relative mb-[10%] text-center">MEET</p>
-        <p className="relative mb-[10%] text-center">THE</p>
-        <p className="relative text-center">BOARD</p>
+      </div>
+
+      <div className="w-2/3 relative">
+        <div className="absolute inset-0 w-full h-full">
+          <Image
+            src="/board/boardGroupImage.webp"
+            alt="board group image"
+            layout="fill"
+            objectFit="cover"
+            className="brightness-50"
+          />
+        </div>
+
+        <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <p className="font-caveat-brush text-3xl text-white md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-8xl">
+            BOARD
+          </p>
+        </div>
       </div>
     </div>
   );
