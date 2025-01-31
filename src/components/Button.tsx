1853c6dbcd4987ec90a747dcd5628758b35777e4
amir <nintendothing123@gmail.com>
2025-01-31 14:50:35 -0800

    added responsiveness to sparring

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,12 +7,18 @@ const Button = ({
   link,
   borderColor = "grey", //defaults to "grey" border class if no colors provided
   bgColor = "grey", //defaults to "grey" bg class if no colors provided
+  smVariant = "small",
+  lgVariant = "large",
+  mdVariant = "medium",
 }: {
   txt: string;
   target?: string;
   link: string;
   borderColor?: "red" | "blue" | "grey";
   bgColor?: "red" | "blue" | "grey";
+  smVariant?: "small";
+  lgVariant?: "large";
+  mdVariant?: "medium";
 }) => {
   //dyanmic props to bg colors and border colors
   const bgVariant = {
@@ -26,15 +32,30 @@ const Button = ({
     blue: "border-tkd-blue-300 hover:border-opacity-90",
   };
 
+  const screenSmVariant = {
+    small: "sm:border-1  sm:px-1 sm:py-1 sm:text-sm  ",
+  };
+
+  const screenLgVariant = {
+    large: "lg:px-7 lg:py-7 lg:text-3xl",
+  };
+
+  const screenMdVariant = {
+    medium: "md:px-5 md:py-5 md:text-2xl md:border-3",
+  };
+
   //fall back to default colors if no provided colors
   const bgClass = bgVariant[bgColor];
   const borderClass = borderVariant[borderColor];
+  const smClass = screenSmVariant[smVariant];
+  const lgClass = screenLgVariant[lgVariant];
+  const mdClass = screenMdVariant[mdVariant];
 
   return (
     <Link
       href={link}
       target={target}
-      className={`box-border flex items-center justify-center border-4 border-solid px-7 py-7 font-inika text-xl font-medium lg:text-3xl ${bgClass} ${borderClass} `}
+      className={`box-border flex items-center justify-center border-4 border-solid font-inika font-medium ${bgClass} ${borderClass} ${smClass} ${lgClass} ${mdClass} `}
     >
       <div className="relative inline-block whitespace-nowrap">
         <span className="flex justify-center text-white opacity-100">
--- src/components/home/SparringGear.tsx
+++ src/components/home/SparringGear.tsx
@@ -10,8 +10,8 @@ const SparringGear = () => {
         alt="sparringGearPhoto"
         className="w-screen brightness-50"
       />
-      <div className="absolute inset-0 flex flex-col items-center justify-center space-y-5 lg:space-y-20">
-        <div>Need Sparring Gear</div>
+      <div className="absolute inset-0 flex flex-col items-center justify-center space-y-1 lg:space-y-20">
+        <div className="text-2xl md:text-4xl">Need Sparring Gear</div>
         <div className="">
           <Button
             txt="Need Sparring Gear"
@@ -19,11 +19,15 @@ const SparringGear = () => {
             bgColor="red"
             borderColor="red"
             target="_blank"
+            smVariant="small"
+            lgVariant="large"
           />
         </div>
         <br></br>
-        <div className="absolute -bottom-[-5%] right-5 flex w-1/2 flex-col items-end space-y-4 md:text-2xl lg:right-20 lg:text-4xl">
-          <div className="mx-10">Want a Jacket?</div>
+        <div className="absolute -bottom-[-5%] right-5 flex w-1/2 flex-col items-end sm:space-y-2 md:text-2xl lg:right-20 lg:space-y-4 lg:text-4xl">
+          <div className="sm:mx-5 sm:text-2xl md:mx-0 md:text-3xl lg:mx-10">
+            Want a Jacket?
+          </div>
           <div>
             <Button
               txt="Purchase Jacket"
@@ -31,6 +35,8 @@ const SparringGear = () => {
               bgColor="red"
               borderColor="red"
               target="_blank"
+              smVariant="small"
+              lgVariant="large"
             />
           </div>
         </div>
