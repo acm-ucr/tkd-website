f371aeda55b6e6edf68f1c775df9600b6b8b518d
Rebecca Jennings <157240153+Rebecca-J7@users.noreply.github.com>
2025-02-01 20:12:42 -0800

    Made navbar bigger

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,35 +4,37 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <nav className="item-center fixed left-0 top-0 z-50 flex w-full justify-between bg-tkd-blue-300 p-4 text-white">
+    <nav className="item-center fixed left-0 top-0 z-50 flex w-full justify-between bg-tkd-blue-300 p-3 text-white">
       <div className="flex items-center">
         <Link href="/" className="flex">
           <Image
             src={Logo}
             alt="TKD Logo"
-            className="mr-2 h-10 w-10 rounded-full"
+            className="h-27 w-27 mr-7 rounded-full"
           />
-          <span className="title font-jockey-one text-2xl uppercase">TKD</span>
+          <span className="title flex items-center font-jockey-one text-5xl uppercase">
+            TKD
+          </span>
         </Link>
       </div>
-      <ul className="flex space-x-6">
+      <ul className="flex items-center space-x-6">
         <li>
-          <Link href="/about" className="font-jockey-one text-2xl uppercase">
+          <Link href="/about" className="font-jockey-one text-5xl uppercase">
             About
           </Link>
         </li>
         <li>
-          <Link href="/events" className="font-jockey-one text-2xl uppercase">
+          <Link href="/events" className="font-jockey-one text-5xl uppercase">
             Events
           </Link>
         </li>
         <li>
-          <Link href="/board" className="font-jockey-one text-2xl uppercase">
+          <Link href="/board" className="font-jockey-one text-5xl uppercase">
             Board
           </Link>
         </li>
         <li>
-          <Link href="/gallery" className="font-jockey-one text-2xl uppercase">
+          <Link href="/gallery" className="font-jockey-one text-5xl uppercase">
             Gallery
           </Link>
         </li>
