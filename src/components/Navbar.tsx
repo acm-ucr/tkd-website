db46f21d43f33fc92ddb7fe8e12fcf3aa8300def
Rebecca Jennings <157240153+Rebecca-J7@users.noreply.github.com>
2025-02-02 22:00:22 -0800

    adjusted navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,37 +4,37 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <nav className="item-center fixed left-0 top-0 z-50 flex w-full justify-between bg-tkd-blue-300 p-3 text-white">
+    <nav className="item-center sticky left-0 top-0 z-50 flex w-full justify-between bg-tkd-blue-300 p-3 px-5 text-white">
       <div className="flex items-center">
         <Link href="/" className="flex">
           <Image
             src={Logo}
             alt="TKD Logo"
-            className="h-27 w-27 mr-7 rounded-full"
+            className="mr-7 h-20 w-20 rounded-full"
           />
-          <span className="title flex items-center font-jockey-one text-5xl uppercase">
+          <span className="title flex items-center font-jockey-one text-4xl uppercase">
             TKD
           </span>
         </Link>
       </div>
       <ul className="flex items-center space-x-6">
         <li>
-          <Link href="/about" className="font-jockey-one text-5xl uppercase">
+          <Link href="/about" className="font-jockey-one text-4xl uppercase">
             About
           </Link>
         </li>
         <li>
-          <Link href="/events" className="font-jockey-one text-5xl uppercase">
+          <Link href="/events" className="font-jockey-one text-4xl uppercase">
             Events
           </Link>
         </li>
         <li>
-          <Link href="/board" className="font-jockey-one text-5xl uppercase">
+          <Link href="/board" className="font-jockey-one text-4xl uppercase">
             Board
           </Link>
         </li>
         <li>
-          <Link href="/gallery" className="font-jockey-one text-5xl uppercase">
+          <Link href="/gallery" className="font-jockey-one text-4xl uppercase">
             Gallery
           </Link>
         </li>
