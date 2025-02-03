6f5451fb63f20b776c29adae5097d6abf866641e
Yuvika <yuvikashiv03@gmail.com>
2025-02-02 23:33:28 -0800

    added animation our mission

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,25 @@
+"use client";
 import AdditionalInfo from "@/components/about/AdditionalInfo";
 import Mission from "@/components/about/Mission";
+import { motion } from "framer-motion";
 
 const About = () => {
   return (
     <div className="">
-      <Mission />
+      <motion.div
+        initial={{ opacity: 0, y: 50, scale: 0.8 }}
+        animate={{ opacity: 1, y: 0, scale: 1 }}
+        transition={{
+          duration: 1,
+          ease: "easeOut",
+          type: "spring",
+          stiffness: 100,
+        }}
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+      >
+        <Mission />
+      </motion.div>
       <AdditionalInfo />
     </div>
   );
