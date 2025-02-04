dc8424240c1b36cc63686a85a2fae71f6c737986
Andrew Chen <andrewseanchen@gmail.com>
2025-02-04 12:24:39 -0800

    eventlanding

--- src/components/events/Landing.tsx
+++ src/components/events/Landing.tsx
@@ -1,5 +1,12 @@
+import Image from "next/image";
+import landing from "@/../public/events/eventlanding.webp";
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div className="relative flex items-center justify-center">
+      <Image className="w-screen" src={landing} alt="Landing" />
+    </div>
+  );
 };
 
 export default Landing;
