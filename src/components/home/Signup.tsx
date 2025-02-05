5d32be77f2c6946977d02c93969f9b64d1c6e2b5
Haokun Yang <haokunyang@ucr-secure-20-10-13-131-43.wnet.ucr.edu>
2025-02-04 19:58:23 -0800

    text size change for MR, forgot to format

--- src/components/home/Signup.tsx
+++ src/components/home/Signup.tsx
@@ -10,8 +10,8 @@ const Signup = () => {
       </div>
       <div className="flex flex-col items-center gap-4 md:flex-row md:gap-[5%] md:justify-self-center">
         <div className="w-full md:w-[50%]">
-          <div className="mb-4 break-words font-inika text-black sm:text-xl sm:mb-6 lg:mb-8 lg:text-3xl xl:text-5xl md:leading-relaxed">
-            blahblahblahblahblahblahblahblahh blahblahblah blahblahblah
+          <div className="mb-4 break-words font-inika text-black sm:mb-6 sm:text-xl md:leading-relaxed lg:mb-8 lg:text-3xl xl:text-5xl">
+            blahblahblahblahblahblahblahblahh blahblahblah blahblahblahh
           </div>
           <div className="sm: flex justify-center justify-self-center py-[4%] lg:py-[8%]">
             <SignUpButton
