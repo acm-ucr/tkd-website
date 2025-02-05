4865c649b598f367e33369a9c9c90d62b389dedc
Haokun Yang <haokunyang@ucr-secure-20-10-13-131-43.wnet.ucr.edu>
2025-02-04 19:57:37 -0800

    text size change for MR

--- src/components/home/Signup.tsx
+++ src/components/home/Signup.tsx
@@ -10,16 +10,16 @@ const Signup = () => {
       </div>
       <div className="flex flex-col items-center gap-4 md:flex-row md:gap-[5%] md:justify-self-center">
         <div className="w-full md:w-[50%]">
-          <div className="mb-4 break-words font-inika text-black md:mb-8 md:text-5xl md:leading-relaxed">
+          <div className="mb-4 break-words font-inika text-black sm:text-xl sm:mb-6 lg:mb-8 lg:text-3xl xl:text-5xl md:leading-relaxed">
             blahblahblahblahblahblahblahblahh blahblahblah blahblahblah
           </div>
-          <div className="sm: flex justify-center justify-self-center py-[8%]">
+          <div className="sm: flex justify-center justify-self-center py-[4%] lg:py-[8%]">
             <SignUpButton
               txt="Sign Up"
               link="/some-page"
               borderColor="blue"
               bgColor="blue"
-              textSize="text-lg md:text-5xl"
+              textSize="sm:text-2xl lg:text-3xl xl:text-5xl"
             />
           </div>
         </div>
