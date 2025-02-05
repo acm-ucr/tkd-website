18be3029aee58de264911d4b1c5ff16c3a03390b
Andrew Chen <andrewseanchen@gmail.com>
2025-02-04 19:17:57 -0800

    eventlanding

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -3,7 +3,7 @@ import Landing from "@/components/events/Landing";
 const Events = () => {
   return (
     <div className="flex h-screen w-full items-center justify-center">
-      <Landing/>
+      <Landing />
     </div>
   );
 };
