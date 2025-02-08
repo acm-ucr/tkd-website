dd6e6a3f014f326ac81f2de6a49d0a19fefec77c
Matthew Garcia <gmatthew2765@gmail.com>
2025-02-07 19:48:36 -0800

    Fixed positioning and padding of buttons

--- src/components/home/SparringGear.tsx
+++ src/components/home/SparringGear.tsx
@@ -11,7 +11,7 @@ const SparringGear = () => {
         className="w-screen brightness-50"
       />
       <div className="absolute inset-0 flex flex-col items-center justify-center space-y-1 lg:space-y-20">
-        <div className="mb-4 text-center text-xl md:text-3xl lg:text-6xl">
+        <div className="mb-4 text-center text-xl max-sm:text-2xl md:text-3xl lg:text-6xl">
           Need Sparring Gear?
         </div>
         <div>
@@ -23,16 +23,16 @@ const SparringGear = () => {
             target="_blank"
             smVariant="small"
             lgVariant="large"
-            padding="px-[60%] py-[10%]"
+            padding="px-[60%] py-[10%] max-md:px-[55%] max-md:py-[5%]"
           />
         </div>
         <br></br>
         <br></br>
         <div className="absolute -bottom-[-5%] right-5 flex w-1/2 flex-col items-end sm:space-y-2 md:text-2xl lg:right-20 lg:space-y-4 lg:text-4xl">
-          <div className="sm:mx-5 sm:text-xl md:mx-0 md:text-3xl lg:-mx-8 lg:text-5xl">
+          <div className="relative max-sm:right-6 sm:right-7 sm:mx-5 sm:text-xl md:mx-0 md:text-3xl lg:-mx-8 lg:text-5xl">
             Want a Jacket?
           </div>
-          <div className="relative max-sm:right-1">
+          <div className="relative max-sm:right-5 sm:right-8 md:right-8">
             <Button
               txt="Purchase Jacket"
               link=""
@@ -42,7 +42,7 @@ const SparringGear = () => {
               smVariant="small"
               lgVariant="large"
               textSize="max-sm:text-xs"
-              padding="px-[60%] py-[5%] max-sm:py-[2%] max-sm:px-[50%]"
+              padding="px-[60%] py-[5%]"
             />
           </div>
         </div>
