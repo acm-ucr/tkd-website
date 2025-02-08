7ac343d4a70fcb12a6e5f97f682bc0478788f86c
Matthew Garcia <gmatthew2765@gmail.com>
2025-02-07 20:14:55 -0800

    Fixed text size at larger screens

--- src/components/home/SparringGear.tsx
+++ src/components/home/SparringGear.tsx
@@ -10,8 +10,8 @@ const SparringGear = () => {
         alt="sparringGearPhoto"
         className="w-screen brightness-50"
       />
-      <div className="absolute inset-0 flex flex-col items-center justify-center space-y-1 lg:space-y-20">
-        <div className="mb-4 text-center text-xl max-md:text-3xl max-sm:text-2xl md:text-3xl lg:text-6xl">
+      <div className="absolute inset-0 flex flex-col items-center justify-center space-y-1 lg:space-y-12">
+        <div className="mb-4 text-center text-xl max-md:text-3xl max-sm:mb-0 max-sm:text-lg md:text-3xl lg:text-5xl">
           Need Sparring Gear?
         </div>
         <div>
@@ -23,13 +23,14 @@ const SparringGear = () => {
             target="_blank"
             smVariant="small"
             lgVariant="large"
-            padding="px-[60%] py-[10%] max-md:px-[55%] max-md:py-[5%]"
+            textSize="lg:text-3xl max-sm:text-xs"
+            padding="px-[60%] py-[10%] max-md:px-[55%] max-md:py-[5%] max-sm:py-[8%]"
           />
         </div>
         <br></br>
         <br></br>
         <div className="absolute -bottom-[-5%] right-5 flex w-1/2 flex-col items-end sm:space-y-2 md:text-2xl lg:right-20 lg:space-y-4 lg:text-4xl">
-          <div className="relative max-sm:right-10 max-sm:text-[10px] sm:right-7 sm:mx-5 sm:text-xl md:mx-0 md:text-3xl lg:-mx-8 lg:text-5xl">
+          <div className="relative max-sm:right-6 max-sm:text-[10px] sm:right-7 sm:mx-5 sm:text-xl md:mx-0 md:text-3xl lg:right-20 lg:-mx-8 lg:text-2xl">
             Want a Jacket?
           </div>
           <div className="relative max-sm:right-5 sm:right-8 md:right-8">
@@ -41,8 +42,8 @@ const SparringGear = () => {
               target="_blank"
               smVariant="small"
               lgVariant="large"
-              textSize="max-sm:text-[10px]"
-              padding="px-[60%] py-[5%] max-sm:py-[8%]"
+              textSize="lg:text-lg max-sm:text-[8px]"
+              padding="px-[60%] py-[5%] max-sm:px-[55%]"
             />
           </div>
         </div>
