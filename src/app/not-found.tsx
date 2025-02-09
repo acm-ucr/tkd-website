3fabace9d8e21c79a9fc3f2f4281f00da86f899d
arkbot51 <rhahee528@gmail.com>
2025-02-08 20:50:27 -0800

    changed percentages to vw to make screen scaling smoother

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,32 +3,34 @@ import Image from "next/image";
 
 const NotFound = () => {
   return (
-    <div className="relative h-screen w-screen overflow-hidden">
+    <div className="relative flex w-screen flex-col items-center justify-center overflow-hidden">
       <Image
         src="/404page-background.webp"
         width={3024}
         height={1776}
-        className="h-auto max-h-screen w-screen object-cover"
+        className="h-full w-screen object-cover"
         alt=" 404 background image"
       />
-      <div className="absolute left-[50%] top-[50%] z-0 flex h-[55%] w-[60%] translate-x-[-50%] translate-y-[-50%] items-center justify-center bg-tkd-blue-100">
-        <div className="absolute right-[3%] top-[-5%] h-[100%] w-[100%] border-2 border-white bg-transparent">
-          <p className="absolute left-[50%] top-[-15%] translate-x-[-50%] translate-y-[-10%] font-caveat-brush text-[550%] text-white sm:text-[630%] lg:text-[860%]">
+
+      <div className="absolute left-[50%] top-[50%] z-0 flex translate-x-[-50%] translate-y-[-50%] items-center justify-center bg-tkd-blue-100 px-[7%] py-[12%]">
+        <div className="absolute right-[3%] top-[-5%] flex h-[100%] w-[100%] justify-center border-2 border-white bg-transparent">
+          <p className="translate-y-[-20%] justify-center font-caveat-brush text-[9vw] text-white">
             404
           </p>
         </div>
-        <p className="translate-y-[-11%] font-inika text-[200%] text-white sm:text-[350%] lg:text-[550%]">
+        <p className="translate-y-[-11%] text-nowrap font-inika text-[5vw] text-white">
           PAGE NOT FOUND
         </p>
       </div>
-      <div className="absolute left-[50%] top-[82%] h-[12%] w-[22%] translate-x-[-50%]">
+
+      <div className="absolute left-[50%] top-[78%] flex translate-x-[-50%] justify-center">
         <Button
           txt="Back To Home"
           link="/"
           borderColor="red"
           bgColor="red"
-          size="w-[100%] h-[100%]"
-          textSize="text-[100%] sm:text-[170%] lg:text-[250%]"
+          size="px-[16%] py-[5%]"
+          textSize="text-[2.5vw]"
         />
       </div>
     </div>
