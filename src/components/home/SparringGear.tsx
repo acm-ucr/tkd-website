785aad822d695cc9ef8b08d559b73dde9f66d4f2
Matthew Garcia <gmatthew2765@gmail.com>
2025-02-10 08:35:54 -0800

    fixed right positioning and gap between text and button

--- src/components/home/SparringGear.tsx
+++ src/components/home/SparringGear.tsx
@@ -10,8 +10,8 @@ const SparringGear = () => {
         alt="sparringGearPhoto"
         className="w-screen brightness-50"
       />
-      <div className="absolute inset-0 flex flex-col items-center justify-center space-y-1 lg:space-y-12">
-        <div className="mb-4 text-center text-xl max-md:text-3xl max-sm:mb-0 max-sm:text-lg md:text-3xl lg:text-5xl">
+      <div className="absolute inset-0 flex flex-col items-center justify-center space-y-1 lg:space-y-5">
+        <div className="mb-4 text-center max-md:text-3xl max-sm:mb-0 max-sm:text-lg md:text-5xl lg:text-5xl">
           Need Sparring Gear?
         </div>
         <div>
@@ -29,7 +29,7 @@ const SparringGear = () => {
         </div>
         <br></br>
         <br></br>
-        <div className="absolute -bottom-[-5%] right-5 flex w-1/2 flex-col items-end sm:space-y-2 md:text-2xl lg:right-20 lg:space-y-4 lg:text-4xl">
+        <div className="absolute -bottom-[-5%] flex w-1/2 flex-col items-end max-lg:right-8 max-md:right-6 max-sm:right-1 sm:space-y-2 md:text-2xl lg:right-7 lg:space-y-4 lg:text-4xl">
           <div className="relative max-sm:right-6 max-sm:text-[10px] sm:right-7 sm:mx-5 sm:text-xl md:mx-0 md:text-3xl lg:right-20 lg:-mx-8 lg:text-2xl">
             Want a Jacket?
           </div>
