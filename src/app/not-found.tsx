eacb7a01953f758151a493c833da46e0ec425ef6
arkbot51 <rhahee528@gmail.com>
2025-02-11 21:21:15 -0800

    changed image scaling, made divs bigger on mobile

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,30 +8,32 @@ const NotFound = () => {
         src="/404page-background.webp"
         width={3024}
         height={1776}
-        className="h-full w-screen object-cover"
+        className="min-h-screen w-screen object-cover"
         alt=" 404 background image"
       />
 
-      <div className="absolute left-[50%] top-[50%] z-0 flex translate-x-[-50%] translate-y-[-50%] items-center justify-center bg-tkd-blue-100 px-[7%] py-[12%]">
-        <div className="absolute right-[3%] top-[-5%] flex h-[100%] w-[100%] justify-center border-2 border-white bg-transparent">
-          <p className="translate-y-[-20%] justify-center font-caveat-brush text-[9vw] text-white">
-            404
+      <div className="absolute left-[50%] top-[55%] z-0 flex translate-x-[-50%] translate-y-[-50%] flex-col items-center gap-7">
+        <div className="relative flex flex-col items-center justify-center bg-tkd-blue-100 px-[17%] py-[27%]">
+          <div className="absolute left-[-3%] top-[-5%] flex h-full w-full justify-center border-2 border-white bg-transparent">
+            <p className="translate-y-[-20%] justify-center font-caveat-brush text-[13vw] text-white sm:text-[10vw] md:text-[9vw] lg:text-[9vw]">
+              404
+            </p>
+          </div>
+          <p className="translate-y-[-11%] text-nowrap font-inika text-[8vw] text-white sm:text-[6vw] md:text-[5vw] lg:text-[5vw]">
+            PAGE NOT FOUND
           </p>
         </div>
-        <p className="translate-y-[-11%] text-nowrap font-inika text-[5vw] text-white">
-          PAGE NOT FOUND
-        </p>
-      </div>
 
-      <div className="absolute left-[50%] top-[78%] flex translate-x-[-50%] justify-center">
-        <Button
-          txt="Back To Home"
-          link="/"
-          borderColor="red"
-          bgColor="red"
-          size="px-[16%] py-[5%]"
-          textSize="text-[2.5vw]"
-        />
+        <div className="flex justify-center">
+          <Button
+            txt="Back To Home"
+            link="/"
+            borderColor="red"
+            bgColor="red"
+            size="px-[16%] py-[5%]"
+            textSize="text-[4vw] sm:text-[3vw] md:text-[2.5vw] lg:text-[2.5vw]"
+          />
+        </div>
       </div>
     </div>
   );
