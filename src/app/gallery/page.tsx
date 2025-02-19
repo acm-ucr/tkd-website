7469a12254a6ae8807ee38cfab0084a147f5e692
Luke Baring <baringluke35@gmail.com>
2025-02-18 19:20:31 -0800

    fixed extra white space at the top

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -2,7 +2,7 @@ import Landing from "@/components/gallery/Landing";
 
 const Gallery = () => {
   return (
-    <div className="flex h-screen w-full items-center justify-center">
+    <div className="">
       <Landing />
     </div>
   );
--- src/components/gallery/Landing.tsx
+++ src/components/gallery/Landing.tsx
@@ -3,7 +3,7 @@ import landing from "@/../public/gallery/landing-image.webp";
 
 const Landing = () => {
   return (
-    <div className="relative flex flex-row items-center justify-center text-[8vw]">
+    <div className="relative flex flex-col justify-center text-[8vw]">
       <Image className="w-screen" src={landing} alt="Landing" />
       <div className="absolute inset-0 flex flex-col items-center font-caveat-brush">
         <p className="relative text-white max-sm:top-[35%] sm:top-[35%] md:left-[25%] md:top-[7%]">
