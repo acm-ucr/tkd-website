18a94ffd6271450fe7f68ef2fad64bdc99222003
arkbot51 <rhahee528@gmail.com>
2025-02-21 01:07:43 -0800

    animated home landing page

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -1,6 +1,8 @@
+"use client";
 import React from "react";
 import BackgroundImage from "@/../public/home/postcss.config.webp";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const Landing = () => {
   return (
@@ -12,10 +14,32 @@ const Landing = () => {
       />
       <div className="items-center text-white">
         <div className="absolute top-[9%] -mx-7 font-caveat-brush text-9xl max-lg:left-0 max-lg:text-8xl max-md:left-[13%] max-md:top-[25%] max-md:text-6xl max-sm:left-[15%] max-sm:text-[3rem] md:mx-16 lg:mx-8 lg:text-9xl">
-          TAEKWONDO @ UCR
+          <motion.div
+            initial={{ opacity: 0, x: 90 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{
+              duration: 0.75,
+              ease: "easeOut",
+              delay: 0.2,
+              bounce: 0.3,
+            }}
+          >
+            TAEKWONDO @ UCR
+          </motion.div>
         </div>
         <div className="max-sm:text-md absolute bottom-[8%] right-[4%] font-inika text-5xl max-lg:text-3xl max-md:bottom-[18%] max-md:text-base">
-          Welcome to Taekwondo Club at UCR!
+          <motion.div
+            initial={{ opacity: 0, x: -60 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{
+              duration: 0.75,
+              ease: "easeOut",
+              delay: 0.5,
+              bounce: 0.3,
+            }}
+          >
+            Welcome to Taekwondo Club at UCR!
+          </motion.div>
         </div>
       </div>
     </div>
