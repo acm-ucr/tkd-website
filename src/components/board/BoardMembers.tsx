7318c3e2ce6cb54518ba2fb3fa249d48736d746b
Jonathan Le <jonathanle062303@gmail.com>
2025-03-01 10:22:43 -0800

    Destructure board member card prop

--- src/components/board/BoardMembers.tsx
+++ src/components/board/BoardMembers.tsx
@@ -3,12 +3,14 @@ import { members } from "@/data/BoardData";
 
 const BoardMembers = () => {
   return (
-    <div
-      id="board"
-      className="flex flex-wrap justify-center gap-20 px-8 py-[8%] max-sm:px-5"
-    >
+    <div className="flex flex-wrap justify-center gap-20 px-8 py-[8%] max-sm:px-5">
       {members.map((member, id) => (
-        <BoardMemberCard key={id} member={member} />
+        <BoardMemberCard
+          key={id}
+          url={member.url}
+          position={member.position}
+          name={member.name}
+        />
       ))}
     </div>
   );
