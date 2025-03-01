17392f925cee2b8fc7358ee68e7fcba42053f988
Jonathan Le <jonathanle062303@gmail.com>
2025-03-01 09:26:29 -0800

    Fix footer responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,23 @@
 import Image from "next/image";
 import tkdicon from "/public/tkd-icon.webp";
 import Link from "next/link";
-import { Facebook } from "lucide-react";
-import { Instagram } from "lucide-react";
-import { SiDiscord } from "@icons-pack/react-simple-icons";
+import {
+  SiFacebook,
+  SiInstagram,
+  SiDiscord,
+} from "@icons-pack/react-simple-icons";
 
 const Footer = () => {
   return (
     <footer className="relative h-auto w-full bg-tkd-blue-300 font-inika">
-      <div className="hidden items-center justify-around py-[6.5%] sm:visible sm:flex">
+      <div className="hidden items-center justify-around py-[6.5%] sm:visible md:flex">
         <div className="flex h-[25%] w-[30%] justify-around font-inika max-lg:gap-[2rem] sm:text-xs lg:text-base xl:text-2xl">
           <Link
             href="https://www.facebook.com/tkducr/"
             className="flex h-auto flex-col items-center justify-items-center sm:w-[20%] lg:w-[20%] xl:w-[18%]"
           >
             <div className="flex aspect-square items-center justify-center rounded-full bg-tkd-grey-100 p-[28%]">
-              <Facebook size={48} className="mr-[3%] text-tkd-blue-300" />
+              <SiFacebook size={48} className="mr-[3%] text-tkd-blue-300" />
             </div>
             <p className="my-[20%] text-white hover:underline">facebook</p>
           </Link>
@@ -24,7 +26,7 @@ const Footer = () => {
             className="flex h-auto flex-col items-center justify-items-center sm:w-[20%] lg:w-[20%] xl:w-[18%]"
           >
             <div className="flex aspect-square items-center justify-center rounded-full bg-tkd-grey-100 p-[28%]">
-              <Instagram size={48} className="text-tkd-blue-300" />
+              <SiInstagram size={48} className="text-tkd-blue-300" />
             </div>
             <p className="my-[20%] text-white hover:underline">instagram</p>
           </Link>
@@ -40,12 +42,12 @@ const Footer = () => {
           </Link>
         </div>
 
-        <div className="absolute bg-white sm:h-[50%] sm:w-[.2%] lg:h-[40%] lg:w-[.12%]"></div>
+        <div className="absolute bg-white sm:h-[50%] sm:w-[.2%] lg:h-[40%] lg:w-[.12%]" />
 
         <div className="w-[31%]">
-          <div className="mr-[50%] w-full overflow-hidden font-inika text-[1.625rem] text-white">
+          <div className="text-md mr-[50%] w-full overflow-hidden font-inika text-white lg:text-2xl">
             <p className="mb-[2%] text-3xl font-bold">Contact Information:</p>
-            <p className="mb-[2%] font-[700]">Address:</p>
+            <p className="font-[700]">Address:</p>
             <p>
               1100 University Ave
               <br />
@@ -53,7 +55,7 @@ const Footer = () => {
               <br />
               United States
             </p>
-            <p className="my-[2%] font-[700]">Contact Email:</p>
+            <p className="mt-[2%] font-[700]">Contact Email:</p>
             <p>ucrtaekwondo@gmail.com</p>
           </div>
 
@@ -65,14 +67,14 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="visibile flex flex-col justify-center py-[7%] sm:hidden">
+      <div className="visibile flex flex-col justify-center py-[7%] md:hidden">
         <div className="mb-[5%] flex w-full justify-center gap-2">
           <Link
             href="/"
             className="flex h-auto w-[16%] flex-col items-center justify-items-center"
           >
             <div className="flex aspect-square items-center justify-center rounded-full bg-tkd-grey-100 p-[16%]">
-              <Facebook size={18} className="mr-[3%] text-tkd-blue-300" />
+              <SiFacebook size={24} className="mr-[3%] text-tkd-blue-300" />
             </div>
           </Link>
 
@@ -81,7 +83,7 @@ const Footer = () => {
             className="mx-[-5%] flex h-auto w-[16%] flex-col items-center justify-items-center"
           >
             <div className="flex aspect-square items-center justify-center rounded-full bg-tkd-grey-100 p-[16%]">
-              <Instagram size={18} className="text-tkd-blue-300" />
+              <SiInstagram size={24} className="text-tkd-blue-300" />
             </div>
           </Link>
 
@@ -90,7 +92,7 @@ const Footer = () => {
             className="flex h-auto w-[16%] flex-col items-center justify-items-center"
           >
             <div className="flex aspect-square items-center justify-center rounded-full bg-tkd-grey-100 p-[16%]">
-              <SiDiscord size={18} className="text-tkd-blue-300" />
+              <SiDiscord size={24} className="text-tkd-blue-300" />
             </div>
           </Link>
         </div>
@@ -99,7 +101,7 @@ const Footer = () => {
           <div className="absolute h-[1%] w-[70%] bg-white"></div>
         </div>
 
-        <div className="mt-[4%] flex h-[50%] w-full items-center justify-center text-[0.65rem] text-white">
+        <div className="mt-[4%] flex h-[50%] w-full items-center justify-center text-[0.65rem] text-white sm:text-sm">
           <div>
             <p className="mb-[2%] font-bold">Contact Information:</p>
             <p>
