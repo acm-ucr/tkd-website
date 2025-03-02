f1e2951d267249276720cbc486f33c32d8ef33d5
Yuvika <yuvikashiv03@gmail.com>
2025-03-01 23:41:51 -0800

    added animation

--- src/components/about/Mission.tsx
+++ src/components/about/Mission.tsx
@@ -1,20 +1,34 @@
+"use client";
+import dynamic from "next/dynamic";
+import { motion } from "framer-motion";
+
 const Mission = () => {
   return (
     <div className="flex w-full flex-col items-center py-[6%]">
-      <p className="m-2 mb-8 text-center font-inika text-4xl font-bold sm:text-5xl sm:leading-[95%]">
+      <motion.p
+        className="m-2 mb-8 text-center font-inika text-4xl font-bold sm:text-5xl sm:leading-[95%]"
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, type: "spring", stiffness: 60 }}
+      >
         OUR MISSION
-      </p>
+      </motion.p>
 
-      <p className="m-0 w-[80%] text-center font-inika text-xl sm:m-3 sm:w-[57%] sm:text-3xl sm:leading-[139%]">
+      <motion.p
+        className="m-0 w-[80%] text-center font-inika text-xl sm:m-3 sm:w-[57%] sm:text-3xl sm:leading-[139%]"
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, delay: 0.3, type: "spring", stiffness: 80 }}
+      >
         The mission of the Taekwondo Club is to provide training for those of
         all skill levels. We welcome all those who have previous experience,
         zero experience, are martial arts enthusiasts, or generally curious of
         the sport. We aim to teach the rich Korean culture of Taekwondo to all
         members, as well as the development of competitive skills while having
         fun.
-      </p>
+      </motion.p>
     </div>
   );
 };
 
-export default Mission;
+export default dynamic(() => Promise.resolve(Mission), { ssr: false });
