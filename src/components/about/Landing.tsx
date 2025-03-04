54702a6e3eabb3ecfe9bc5ab83dabe3f657c7e77
HaokunYang <t300025hao@gmail.com>
2025-03-04 11:10:05 -0800

    MR for about landing

--- src/components/about/Landing.tsx
+++ src/components/about/Landing.tsx
@@ -6,15 +6,15 @@ const Landing = () => {
     <div className="relative w-full">
       <Image
         src={Background}
-        className="w-full aspect-video object-cover brightness-50"
+        className="aspect-video w-full object-cover brightness-50"
         alt="Landing background image"
       />
 
-      <div className="absolute flex items-center justify-center md:items-start md:justify-start inset-0 md:inset-auto md:top-[7%] md:left-[7%] z-10 font-caveat-brush text-5xl sm:text-6xl md:text-7xl lg:text-9xl font-extrabold uppercase text-white">
+      <div className="absolute inset-0 z-10 flex items-center justify-center font-caveat-brush text-5xl font-extrabold uppercase text-white sm:text-6xl md:inset-auto md:left-[7%] md:top-[7%] md:items-start md:justify-start md:text-7xl lg:text-9xl">
         About Us
       </div>
 
-      <p className="absolute bottom-4 right-2 xs:right-3 sm:right-4 md:bottom-4 md:right-4 z-10 text-right text-xs xs:text-sm sm:text-md md:text-4xl font-medium text-white max-w-[85%] sm:max-w-[75%] md:max-w-none">
+      <p className="xs:right-3 xs:text-sm sm:text-md absolute bottom-4 right-2 z-10 max-w-[85%] text-right text-xs font-medium text-white sm:right-4 sm:max-w-[75%] md:bottom-4 md:right-4 md:max-w-none md:text-4xl">
         Come join us! Our practices are Mondays and <br />
         Wednesdays from 9:30 PM - 10:50 PM.
       </p>
