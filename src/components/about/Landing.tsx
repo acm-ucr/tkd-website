a20480b38451cf3ce85f3b9538b39d6fcc0d4772
HaokunYang <t300025hao@gmail.com>
2025-03-04 11:09:20 -0800

    MR About about landing

--- src/components/about/Landing.tsx
+++ src/components/about/Landing.tsx
@@ -3,18 +3,18 @@ import Background from "@/public/about/about_landing.webp";
 
 const Landing = () => {
   return (
-    <div className="relative">
+    <div className="relative w-full">
       <Image
         src={Background}
-        className="h-[990px] w-screen overflow-hidden object-cover"
+        className="w-full aspect-video object-cover brightness-50"
         alt="Landing background image"
       />
 
-      <div className="absolute left-[13%] top-[2%] z-10 font-caveat-brush text-9xl font-extrabold uppercase text-white">
+      <div className="absolute flex items-center justify-center md:items-start md:justify-start inset-0 md:inset-auto md:top-[7%] md:left-[7%] z-10 font-caveat-brush text-5xl sm:text-6xl md:text-7xl lg:text-9xl font-extrabold uppercase text-white">
         About Us
       </div>
 
-      <p className="absolute bottom-4 right-4 z-10 text-center text-5xl font-medium text-white">
+      <p className="absolute bottom-4 right-2 xs:right-3 sm:right-4 md:bottom-4 md:right-4 z-10 text-right text-xs xs:text-sm sm:text-md md:text-4xl font-medium text-white max-w-[85%] sm:max-w-[75%] md:max-w-none">
         Come join us! Our practices are Mondays and <br />
         Wednesdays from 9:30 PM - 10:50 PM.
       </p>
