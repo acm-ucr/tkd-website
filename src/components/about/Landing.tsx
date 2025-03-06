c6f176bd7dc8d99543b9174897bbdcf25cfde6dd
HaokunYang <t300025hao@gmail.com>
2025-03-06 09:46:07 -0800

    animation for about us landing

--- src/components/about/Landing.tsx
+++ src/components/about/Landing.tsx
@@ -1,23 +1,45 @@
+"use client";
+
 import Image from "next/image";
 import Background from "@/public/about/about_landing.webp";
+import { motion } from "framer-motion";
 
 const Landing = () => {
   return (
     <div className="relative w-full">
-      <Image
-        src={Background}
-        className="aspect-video w-full object-cover brightness-50"
-        alt="Landing background image"
-      />
+      {/* Image with blur-in animation */}
+      <motion.div
+        initial={{ filter: "blur(10px) brightness(0.5)" }}
+        animate={{ filter: "blur(0px) brightness(0.5)" }}
+        transition={{ duration: 2 }}
+      >
+        <Image
+          src={Background}
+          className="aspect-video w-full object-cover"
+          alt="Landing background image"
+        />
+      </motion.div>
 
-      <div className="absolute inset-0 z-10 flex items-center justify-center font-caveat-brush text-5xl font-extrabold uppercase text-white sm:text-6xl md:inset-auto md:left-[7%] md:top-[7%] md:items-start md:justify-start md:text-7xl lg:text-9xl">
+      {/* "About Us" text with fade-in animation */}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2 }}
+        className="absolute inset-0 z-10 flex items-center justify-center font-caveat-brush text-5xl font-extrabold uppercase text-white sm:text-6xl md:inset-auto md:left-[7%] md:top-[7%] md:items-start md:justify-start md:text-7xl lg:text-9xl"
+      >
         About Us
-      </div>
+      </motion.div>
 
-      <p className="xs:right-3 xs:text-sm sm:text-md absolute bottom-4 right-2 z-10 max-w-[85%] text-right text-xs font-medium text-white sm:right-4 sm:max-w-[75%] md:bottom-4 md:right-4 md:max-w-none md:text-4xl">
+      {/* Paragraph text with fade-in animation */}
+      <motion.p
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2 }}
+        className="xs:right-3 xs:text-sm sm:text-md absolute bottom-4 right-2 z-10 max-w-[85%] text-right text-xs font-medium text-white sm:right-4 sm:max-w-[75%] md:bottom-4 md:right-4 md:max-w-none md:text-4xl"
+      >
         Come join us! Our practices are Mondays and <br />
         Wednesdays from 9:30 PM - 10:50 PM.
-      </p>
+      </motion.p>
     </div>
   );
 };
