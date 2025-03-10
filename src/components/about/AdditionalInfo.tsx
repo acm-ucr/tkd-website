0de92c5c846c1e34f34b46d76ede5201fa3cf26f
Andrew Chen <andrewseanchen@gmail.com>
2025-03-09 19:26:40 -0700

    text

--- src/components/about/AdditionalInfo.tsx
+++ src/components/about/AdditionalInfo.tsx
@@ -3,7 +3,7 @@ import Background from "@/public/about/AdditionalInfo.webp";
 
 const AdditionalInfo = () => {
   return (
-    <div className="relative lg:h-full lg:w-full md:h-full md:w-full sm:h-full sm:w-full h-max w-fit">
+    <div className="relative h-max w-fit sm:h-full sm:w-full md:h-full md:w-full lg:h-full lg:w-full">
       <Image
         src={Background}
         alt="background image"
@@ -17,7 +17,7 @@ const AdditionalInfo = () => {
 
       <div className="absolute right-[1%] top-[30%] flex h-[60%] w-[95%] bg-tkd-blue-200 sm:h-[50%] sm:w-[60%] md:h-[45%] md:w-[60%] lg:h-[40%] lg:w-[55%]">
         <div className="relative -top-[9%] right-[3%] flex h-[100%] w-[120%] justify-center border-2 border-white bg-transparent sm:h-[100%] sm:w-[100%] md:h-[100%] md:w-[100%] md:border-4 lg:h-[100%] lg:w-[100%] lg:border-4">
-          <div className="relative left-[2%] top-[26%] text-[2.35vw] text-white justify-items-center sm:text-[1.4vw] md:text-[1.3vw] lg:text-[1.3vw]">
+          <div className="relative left-[2%] top-[26%] justify-items-center text-[2.35vw] text-white sm:text-[1.4vw] md:text-[1.3vw] lg:text-[1.3vw]">
             <p className="font-semibold">
               How often does your student organization meet?
             </p>
@@ -27,7 +27,8 @@ const AdditionalInfo = () => {
               What day and time does your student organization generally meet?
             </p>
             <p className="font-medium">
-              Mondays and Wednesdays from 9:15pm-10:55pm, and Fridays from 6:00pm-6:55pm.
+              Mondays and Wednesdays from 9:15pm-10:55pm, and Fridays from
+              6:00pm-6:55pm.
             </p>
           </div>
         </div>
