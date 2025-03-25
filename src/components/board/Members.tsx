6795b149bfe242914e52b00b7bfe157748aaecd5
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2025-03-25 14:12:23 -0700

    small fix

--- src/components/board/Members.tsx
+++ src/components/board/Members.tsx
@@ -1,7 +1,7 @@
 import MemberCard from "@/components/board/MemberCard";
 import { members } from "@/data/Board";
 
-const BoardMembers = () => {
+const Members = () => {
   return (
     <div className="flex flex-wrap justify-center gap-20 px-8 py-[8%] max-sm:px-5">
       {members.map((member, id) => (
@@ -16,4 +16,4 @@ const BoardMembers = () => {
   );
 };
 
-export default BoardMembers;
+export default Members;
